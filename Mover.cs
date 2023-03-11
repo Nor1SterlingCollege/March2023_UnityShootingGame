69d20358b12aa130346f0d040e2dfabd9548e89c
Tono Lee <tajimanohamikinj-50@yahoo.co.jp>
2023-03-11 16:12:30 -0500

    init commit

---
+++ Mover.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+	public float MaxSpeed = 5f;
+
+	void Update () 
+	{
+		transform.position += transform.forward * MaxSpeed * Time.deltaTime;
+	}
+}
---
+++ PickupScore.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PickupScore : MonoBehaviour 
+{
+	public int ScorePoints = 100;
+	private Text BonusText = null;
+	public float MessageDelay = 2f;
+	public string MessageTag = string.Empty;
+
+ 
+
+	void Awake()
+	{
+		GameObject BonusObject = GameObject.FindGameObjectWithTag(MessageTag);
+		BonusText = BonusObject.GetComponent<Text>();
+ 
+	}
+
+	public void PowerupCollect () 
+	{
+		GameController.Score += ScorePoints;
+
+
+		//Show score text
+		BonusText.enabled=true;
+
+		Invoke("HideText", MessageDelay);
+
+
+	}
+
+	public void HideText()
+	{
+		BonusText.enabled=false;
+	}
+}
